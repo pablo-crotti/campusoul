9f66fa96d8c053226d32a6f4bbc36669c6760c42
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-10-20 14:37:35 +0200

    reset

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,29 @@
 import express from "express";
 const router = express.Router();
+const User = require('../models/user');
 
-router.get("/", (req, res, next) => {
+router.get("/", function (req, res, next) {
   res.send("Got a response from the users route");
 });
 
-router.post("/", (req, res, next) => {
+router.post('/users', async (req, res) => {
+  // try {
+  //   const user = new User(req.body);
+  //   await user.save();
+  //   res.json(user);
+  // } catch (err) {
+  //   res.status(400).json({ error: err.message });
+  // }
   console.log(req.body);
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 export default router;
