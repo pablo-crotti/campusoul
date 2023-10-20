c8fd20b712188e40e22dfb07421b98ac19ebe283
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-10-20 14:51:09 +0200

    reset users

--- models/user.js
+++ models/user.js
@@ -1,28 +1,8 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
-
+// Define the schema for users
 const userSchema = new Schema({
-    name: String,
-    email: String
-    // password: String,
-    // salt: String,
-    // token: String,
-    // birthdate: Date,
-    // bio: String,
-    // location: {
-    //     type: {
-    //         type: String,
-    //         enum: ['Point'],
-    //         required: true
-    //     },
-    //     coordinates: {
-    //         type: [Number],
-    //         required: true
-    //     }
-    // },
-    // last_activity: Date,
-    // created_at: Date
+  name: String
 });
-
-// userSchema.index({ location: '2dsphere' });
+// Create the model from the schema and export it
 export default mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,32 @@
 import express from "express";
-const User = ('../models/user');
+import User from "../models/user.js";
+
 const router = express.Router();
 
-
-router.get("/", function (req, res, next) {
-  res.send("Got a response from the users route");
+router.get('/', function(req, res, next) {
+  User.find()
+    .sort('name')
+    .exec()
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-router.post('/users', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
-  }
+router.post('/', (req, res, next) => {
+  // Create a new document from the JSON in the request body
+  const newUser = new User(req.body);
+  // Save that document
+  newUser.save()
+    .then(savedUser => {
+      // Send the saved document in the response
+      res.send(savedUser);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 export default router;
