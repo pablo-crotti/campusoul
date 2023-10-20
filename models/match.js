7a70ea37ee7bf23a8ad54054fddd75c2be324376
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-10-20 12:56:42 +0200

    Models indent

--- models/match.js
+++ models/match.js
@@ -6,6 +6,6 @@ const matchSchema = new Schema({
     user_2_id: { type: Schema.Types.ObjectId, ref: 'User' },
     user_1_status: Number,
     user_2_status: Number
-  });
+});
 
 export default mongoose.model('Match', matchSchema);
