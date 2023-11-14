6dac64579eaf94f07fcf39fe0b8d2162fd22485e
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-11-14 13:35:03 +0100

    message model

---
+++ models/messageModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  match: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Match',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  read: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
