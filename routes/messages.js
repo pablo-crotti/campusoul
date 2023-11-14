e709ef3df9a187d341f2aaf1660b4f921179853a
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-11-14 13:49:02 +0100

    fix auth

--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import MessageController from '../controllers/messageController.js';
-import authenticate from '../middleware/authMiddleware.js';
+import auth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Envoyer un message
-router.post('/send', authenticate, MessageController.sendMessage);
+router.post('/send', auth, MessageController.sendMessage);
 
 // Récupérer les messages d'un match
-router.get('/:matchId', authenticate, MessageController.getMessages);
+router.get('/:matchId', auth, MessageController.getMessages);
 
 export default router;
