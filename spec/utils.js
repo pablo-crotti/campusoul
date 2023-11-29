26547df41968e683e17fd4e3156ca98c048cbabc
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-11-29 15:52:20 +0100

    fix attribute token

--- spec/utils.js
+++ spec/utils.js
@@ -8,7 +8,7 @@ const signJwt = promisify(jwt.sign);
 // ...
 export async function generateValidJwt(user) {
     const exp = (new Date().getTime() + 7 * 24 * 3600 * 1000) / 1000;
-    const claims = { sub: user._id.toString(), exp: exp };
+    const claims = { _id: user._id.toString(), exp: exp };
     return await signJwt(claims, process.env.JWT_SECRET);
   }
 
