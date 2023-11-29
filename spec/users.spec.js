09583ccde261fe6405f2f6b86e6def82b9bd58c5
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-11-29 16:11:48 +0100

    fix GET route

--- spec/users.spec.js
+++ spec/users.spec.js
@@ -4,24 +4,6 @@ import mongoose from "mongoose"
 import { cleanUpDatabase, generateValidJwt } from "./utils.js"
 import User from "../models/userModel.js"
 
-describe('GET /users', function() {
-    afterEach(cleanUpDatabase);
-    it('should return all users', async function() {
-        const user = await User.create({
-          email: 'test0.test@heig-vd.ch',
-          password: 'Poisson123'
-        });
-
-        const token = await generateValidJwt(user);
-        const res = await supertest(app)
-  
-        .get('/users')
-        .set('Authorization', `Bearer ${token}`)
-        .expect(200)
-        .expect('Content-Type', /json/);
-    });
-});
-
 
 
 describe('POST /users/register', function() {
@@ -37,10 +19,25 @@ describe('POST /users/register', function() {
     });
 });
 
+describe('GET /users', function() {
+  it('should return all users', async function() {
+      const user = await User.create({
+        email: 'test0.test@heig-vd.ch',
+        password: 'Poisson123'
+      });
+
+      const token = await generateValidJwt(user);
+      const res = await supertest(app)
+
+      .get('/users')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .expect('Content-Type', /json/);
+  });
+});
+
+
 describe('POST /users/login', function() {
-
-  afterEach(cleanUpDatabase);
-
   it('should login a user', async function() {
       const res = await supertest(app)
       .post('/users/login')
@@ -54,7 +51,6 @@ describe('POST /users/login', function() {
 )});
 
 describe('PATCH /users/:userId', function() {
-    afterEach(cleanUpDatabase);
     it('should update a user', async function() {
         const user = await User.create({
           email: 'test.test@heig-vd.ch',
