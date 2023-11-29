5c9599c393dfee23bf7246ea2a2b18dfada37bef
Nicolas Meuwly <nicomeuwly@mbp-de-nico.einet.ad.eivd.ch>
2023-11-29 12:39:58 +0100

    Remove commented out code for getAllUsers() function

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,25 +85,6 @@ const userController = {
       res.status(500).json({ message: error.message });
     }
   },
-  /* async getAllUsers(req, res) {
-    try {
-      const page = parseInt(req.query.page) || 1;
-      const limit = 5;
-      const skip = (page - 1) * limit;
-
-      const users = await User.find().skip(skip).limit(limit);
-      const total = await User.countDocuments();
-
-      res.status(200).json({
-        total,
-        page,
-        totalPages: Math.ceil(total / limit),
-        users
-      });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }, */
   async getAllUsers(req, res) {
     try {
       const page = parseInt(req.query.page) || 1;
