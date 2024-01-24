4916de385a6ed9d80bf75ab83ac014455a83090c
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-24 17:43:58 +0100

    msgs

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -10,54 +10,18 @@ const MessageController = {
   * @param {Object} req - The HTTP request object containing the match ID and message content in the body, and the sender's user ID in the user object.
   * @param {Object} res - The HTTP response object for sending back the sent message or an error message.
   */
-  async sendMessage(req, res) {
-    try {
-      const { matchId, content } = req.body;
-      const senderId = req.user._id;
-      const match = await Match.findById(matchId);
-      let receiverId;
-      if (!match) {
-        return res.status(404).json({ message: 'Match not found' });
-      }
-      if (match.users[0] == senderId) {
-        receiverId = match.users[1]
-      } else {
-        receiverId = match.users[0]
-      }
-      const message = new Message({
-        match: matchId,
-        sender: senderId,
-        receiver: receiverId,
-        content: content
-      });
-
-      await message.save();
-      return message;
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-
-  /**
-  * Retrieves all messages within a specific match. The match ID is provided in the request params.
-  * If the match is not found or other errors occur, responds with an appropriate error message.
-  *  
-  * @param {Object} req - The HTTP request object containing the match ID in the params.
-  * @param {Object} res - The HTTP response object for sending back the list of messages or an error message.
-  */
   async getMessages(req, res) {
     try {
       const { matchId } = req.params;
       const userId = req.user._id;
-
-      const userIdString = userId.toString(); 
-
+      const userIdString = userId.toString();
 
       console.log('Match ID:', matchId);
       console.log('User ID:', userIdString);
 
       // Fetch the messages for the matchId.
       const messages = await Message.find({ match: matchId }).populate('sender', 'name').lean();
+      console.log('Messages before update:', messages);
 
       // Update the 'read' status for messages where the current user is the receiver.
       const updateResult = await Message.updateMany(
@@ -75,8 +39,6 @@ const MessageController = {
       res.status(500).json({ message: error.message });
     }
   },
-
-
 
   async getLastMessage(req, res) {
     try {
